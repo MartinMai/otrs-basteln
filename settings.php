05592596c2b03870deaba5161e0a65eb718545b4
Martin Mai <martin.mai@uni-bamberg.de>
2013-03-10 21:58:06 +0100

    +settings

---
+++ settings.php
@@ -0,0 +1,13 @@
+ 
+<?php
+
+
+$conn_data['host']	 = '';
+$conn_data['user'] 	= '';
+$conn_data['pass'] 	= '';
+$conn_data['db'] 	= '';
+
+#cmd  #todo Besipielwerte hinzufügen
+#$output_configs[] = Array(xml_content_key, xml_content_key_replacement)
+
+?>
